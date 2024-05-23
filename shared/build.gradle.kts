05f46a323d33920c725735ada7d0180a95ad50fa
Kevin <vanquel12@gmail.com>
2024-05-23 12:21:30 +0200

    Add mapbox dependencies

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -28,11 +28,23 @@ kotlin {
     sourceSets {
         val commonMain by getting {
             dependencies {
+                implementation("io.ktor:ktor-client-core:1.6.7")
+                implementation("com.squareup.sqldelight:runtime:1.5.3")
             }
         }
         val commonTest by getting {
             dependencies {
                 implementation(kotlin("test"))
+            }
+        }
+        val androidMain by getting {
+            dependencies {
+                implementation("com.mapbox.mapboxsdk:mapbox-android-sdk:9.6.1")
+            }
+        }
+        val iosMain by getting {
+            dependencies {
+                implementation("io.ktor:ktor-client-ios:1.6.7")
             }
         }
     }
